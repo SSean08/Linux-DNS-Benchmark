e2eb38edb1976fbeb718f6030cacc90efba54376
Sean <e03j9rn1@anonaddy.me>
2022-04-07 22:49:14 +0800

    Fixed and refactored program.

--- dns-test.sh
+++ dns-test.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-if [[ $1 -eq "-h" ]];
+if [[ $1 = "-h" ]];
 then
-	echo "dns-test.sh DNSServer hostname queries"
-	echo "ex. ./dns-test.sh 8.8.8.8 facebook.com 100"
+	echo "dns-test.sh dns-server domain-name requests"
+	echo "Example: ./dns-test.sh 8.8.8.8 facebook.com 100"
 else
 
-
-
-
-num=0
+total=0
 queries=$3
 for i in $(seq $3);
 do
 	temp=$( dig @$1 $2 | awk '/Query time:/ { print $4}' )
 	echo Query: $i
 	echo Query "(msec): "$temp
-	num=$(( $num + $temp))
-        echo New Total: $num;
+	total=$(( $total + $temp))
+        echo New Total: $total;
 done
-echo Average "(Total time ($num)/Queries ($queries))": $(($num / $queries))
+echo Average "(Total time ($total) / Queries ($queries))": $(($total / $queries))
 
 fi
